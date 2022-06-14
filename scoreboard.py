d1d1ee94fa4ff578ba08333f3af53fea677e7cdd
Sharath <sharathwelcomesyouin@gmail.com>
2022-06-14 12:34:26 +0530

    fix bugs in high score feature

--- scoreboard.py
+++ scoreboard.py
@@ -1,4 +1,5 @@
 from turtle import Turtle
+
 ALIGNMENT = "center"
 FONT = ("Courier", 12, "normal")
 
@@ -8,12 +9,18 @@ class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
-        self.high_score=0
+        self.high_score = self.get_high_score()
         self.color("white")
         self.penup()
         self.goto(0, 270)
         self.hideturtle()
         self.update_scoreboard()
+
+    def get_high_score(self):
+        # h_score = 0
+        with open("data.txt") as myfile:
+            h_score = int(myfile.read())
+        return h_score
 
     def update_scoreboard(self):
         self.clear()
@@ -25,12 +32,13 @@ class Scoreboard(Turtle):
 
     def score_reset(self):
         if self.score > self.high_score:
-            self.high_score=self.score
-        self.score =0
+            self.high_score = self.score
+            with open("data.txt", mode="w") as myfile:
+                myfile.write(str(self.high_score))
+        self.score = 0
         self.update_scoreboard()
 
     def increase_score(self):
         self.score += 1
 
         self.update_scoreboard()
-
